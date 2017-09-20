dcef03996765a8b91e4853edcd42e5455a3caf78
zhangjianyang <1260883493@qq.com>
2017-09-20 11:05:28 +0800

    add project to git

---
+++ app/src/main/java/com/zjy/tableviewdemo/MainActivity.java
@@ -0,0 +1,74 @@
+package com.zjy.tableviewdemo;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.util.ArrayList;
+import java.util.List;
+
+
+
+public class MainActivity extends AppCompatActivity {
+    private static final String TAG = "MainActivity";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button bt = (Button) findViewById(R.id.test_bt);
+        bt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        TableView tv = (TableView)findViewById(R.id.test_table_view);
+        tv.setHeaderNames("t1","t2","t3","t4","t5","t6","t7","t8");
+        //tv.setHeaderNames("t1","t2","t3");
+        tv.setTableData(getTestData2());
+
+    }
+
+    private List<String[]> getTestData1() {
+        List<String[]> data = new ArrayList<>();
+        data.add(a1);
+        data.add(a2);
+        data.add(a3);
+        data.add(a4);
+        data.add(a5);
+        data.add(a6);
+        data.add(a7);
+        data.add(a8);
+        data.add(a9);
+        data.add(a10);
+        data.add(a11);
+        return data;
+    }
+
+    private String[][] getTestData2() {
+        String[][] data = {a1,a2,a3,a4,a5,a6,a7,a8,a9,a10,a11};
+        return data;
+    }
+
+    String[] a1 = {"111","1","111","11","111111","111","111","11"};
+    String[] a2 = {"2","2222","22","222","22","222","222222","22222"};
+    String[] a3 = {"33333","33","3","33","333","333","3","3333333"};
+    String[] a4 = {"44","444","444","444","4444","44","444","4444"};
+    String[] a5 = {"5","5","5","5","5","5","5","5555555555555555"};
+    String[] a6 = {"666","66","6","6","6","6","6","6666666666666"};
+    String[] a7 = {"777","777","77","","7","","7","777"};
+    String[] a8 = {"8","8","8","8","8","8","8","8"};
+    String[] a9 = {"9","9","999","9999","99","999","9999","q9999999999999999999999999999q"};
+    String[] a10 = {"8","8","888","888888","88","888","8888","888888888"};
+    String[] a11 = {"9","9","999","9999","99","999","9999","999999999999999"};
+
+}
---
+++ app/src/main/java/com/zjy/tableviewdemo/TableView.java
@@ -0,0 +1,216 @@
+package com.zjy.tableviewdemo;
+
+import android.content.Context;
+import android.graphics.Color;
+import android.util.AttributeSet;
+import android.util.Log;
+import android.view.Gravity;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.FrameLayout;
+import android.widget.HorizontalScrollView;
+import android.widget.LinearLayout;
+import android.widget.ListView;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class TableView extends HorizontalScrollView {
+
+    private static final String TAG = "TableView";
+
+    private Context mContext;
+    private RelativeLayout mTableLayout;
+    private FrameLayout mHeaderLayout;
+    private View mDividerView;
+    private ListView mContentListView;
+    private List<String[]> mTableData = new ArrayList<>();
+    private int mColumnCount;
+    private String[] mHeaderNames;
+    private TableAdapter mAdapter;
+
+    private LinearLayout.LayoutParams mItemLayoutParams;
+
+    public TableView(Context context) {
+        this(context, null);
+    }
+
+    public TableView(Context context, AttributeSet attrs) {
+        this(context, attrs, 0);
+    }
+
+    public TableView(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        initVariable();
+        initViews();
+    }
+
+    private void initVariable() {
+        mContext = getContext();
+        mItemLayoutParams = new LinearLayout.LayoutParams(
+                LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT);
+    }
+
+    private void initViews() {
+        View.inflate(mContext, R.layout.table_view_layout, this);
+        mTableLayout = (RelativeLayout)findViewById(R.id.table_layout);
+        mHeaderLayout = (FrameLayout) findViewById(R.id.table_header);
+        mContentListView = (ListView) findViewById(R.id.table_content_list);
+        mDividerView = findViewById(R.id.table_header_divider);
+
+        setBackgroundResource(R.drawable.frame);
+    }
+
+    private LinearLayout createHeader() {
+        LinearLayout header = new LinearLayout(mContext);
+        header.setLayoutParams(mItemLayoutParams);
+
+        for (int i = 0; i < mColumnCount; i++) {
+            TextView view = new TextView(mContext);
+            view.setWidth(100);
+            view.setGravity(Gravity.CENTER_HORIZONTAL);
+            view.setText(mHeaderNames[i]);
+            view.setMaxLines(1);
+            view.setBackgroundResource(R.drawable.right_border);
+            view.setPadding(5, 10, 5, 10);
+            header.addView(view);
+        }
+        return header;
+    }
+
+    private LinearLayout createItem() {
+        LinearLayout item = new LinearLayout(mContext);
+        item.setLayoutParams(mItemLayoutParams);
+        for (int i = 0; i < mColumnCount; i++) {
+            item.addView(createUnitView(100));
+        }
+        return item;
+    }
+
+    private TextView createUnitView(int width) {
+        TextView view = new TextView(mContext);
+        view.setGravity(Gravity.CENTER);
+        view.setWidth(width);
+        view.setMaxLines(1);
+        view.setBackgroundResource(R.drawable.right_border);
+        view.setPadding(5, 10, 5, 10);
+        return view;
+    }
+
+    private void fillTable() {
+        mHeaderLayout.addView(createHeader());
+
+        mDividerView.setBackgroundColor(Color.parseColor("#2c2c2c"));
+        mDividerView.setMinimumWidth(100 * mColumnCount);
+
+        mAdapter = new TableAdapter();
+        mContentListView.setAdapter(mAdapter);
+    }
+
+    @Override
+    protected void onAttachedToWindow() {
+        super.onAttachedToWindow();
+        fillTable();
+    }
+
+    private class TableAdapter extends BaseAdapter {
+
+        private List<String[]> mData;
+        private int mColumnNum;
+
+        TableAdapter() {
+            mColumnNum = mColumnCount;
+            initDataByTableData();
+        }
+
+        private void initDataByTableData() {
+            mData = mTableData;
+        }
+
+        @Override
+        public int getCount() {
+            return mData.size();
+        }
+
+        @Override
+        public String[] getItem(int position) {
+            return mData.get(position);
+        }
+
+        @Override
+        public long getItemId(int position) {
+            return position;
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            if (convertView == null) {
+                convertView = createItem();
+            }
+            ViewGroup itemLayout = ((ViewGroup) convertView);
+
+            String[] data = mData.get(position);
+            for (int i = 0; i < mColumnNum; i++) {
+                View childView = itemLayout.getChildAt(i);
+                if (childView instanceof TextView) {
+                    ((TextView) childView).setText(data[i]);
+                }
+            }
+            return convertView;
+        }
+    }
+
+    private List<String[]> copyData(List<String[]> srcList) {
+        try {
+            ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+            ObjectOutputStream objectOutputStream = new ObjectOutputStream(byteArrayOutputStream);
+            objectOutputStream.writeObject(srcList);
+            String serStr = byteArrayOutputStream.toString("ISO-8859-1");
+            serStr = java.net.URLEncoder.encode(serStr, "UTF-8");
+
+            objectOutputStream.close();
+            byteArrayOutputStream.close();
+
+            String redStr = java.net.URLDecoder.decode(serStr, "UTF-8");
+            ByteArrayInputStream byteArrayInputStream = new ByteArrayInputStream(redStr.getBytes("ISO-8859-1"));
+            ObjectInputStream objectInputStream = new ObjectInputStream(byteArrayInputStream);
+
+            @SuppressWarnings("unchecked")
+            List<String[]> newList = (List<String[]>) objectInputStream.readObject();
+
+            objectInputStream.close();
+            byteArrayInputStream.close();
+
+            return newList;
+        } catch (Exception e) {
+            Log.e(TAG, "copyData: copy list error, Exception=" + e);
+        }
+        return null;
+    }
+
+    public void setHeaderNames(String... names) {
+        if (names == null) {
+            Log.d(TAG, "setHeaderNames: Invalid setting, the param should not be null.");
+            return;
+        }
+        mHeaderNames = names;
+        mColumnCount = mHeaderNames.length;
+    }
+
+    public void setTableData(List<String[]> data) {
+        mTableData = copyData(data);
+    }
+
+    public void setTableData(String[][] data) {
+        setTableData(Arrays.asList(data));
+    }
+
+}
